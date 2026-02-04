b4f43f9d19caa586704588c4e34a8829c6a8254a
Vidya Travel <vidyatravel.vworks@gmail.com>
2026-02-04 16:33:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "visitwonosobo-87316467-f7066",
+  "appId": "1:79591549284:web:22a963bd8fc0550cc77271",
+  "apiKey": "AIzaSyCbpItIexm8_ECymfC6Auwr8_yXF3Ux8Nw",
+  "authDomain": "visitwonosobo-87316467-f7066.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "79591549284"
+};
